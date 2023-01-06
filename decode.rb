b3079cb838c76aa28af739b45fb277bcea89ed26
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2023-01-06 09:47:00 +0100

    Improve code in decode.rb file

--- decode.rb
+++ decode.rb
@@ -10,12 +10,3 @@ def decode_char(morse_char)
   @char = MORSE_CODE_HASH_TABLE[morse_char]
   @char
 end
-
-def decode_word(morse_word)
-  @word = ''
-  @word_array = morse_word.split
-  @word_array.map do |i|
-    @word += decode_char(i)
-  end
-  @word.to_s
-end
